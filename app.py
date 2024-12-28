4c05b44829b84d935444acf914dd9d6dcdb285a5
Дмитрий Афанасьев <wackymc@yandex.ru>
2024-12-28 17:42:52 +0400

    add files

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask, render_template
+
+APP = Flask(__name__)
+HOST = "90.156.225.70"
+PORT = 5000
+SERVICE_NAME = "application"
+ADMIN_NAME = os.environ.get("ADMIN_NAME", "admin")
+
+
+@APP.route("/brigada7")
+def create_table():
+    return render_template("index.html")
+
+
+
+if __name__ == "__main__":
+    APP.run(host=HOST, port=PORT)
